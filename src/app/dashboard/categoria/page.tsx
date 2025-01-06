16b70f8741bfbbea0712a20ec0485e2a0eb499c8
devrainer <rainergabriel70@gmail.com>
2025-01-06 15:39:02 -0300

    fazendo pagina de categorias

--- src/app/dashboard/categoria/page.tsx
+++ src/app/dashboard/categoria/page.tsx
@@ -1,74 +1,33 @@
 "use client";
 import { api } from "@/services/api";
-import { getCookie } from "cookies-next";
 import Button from "../components/button";
 import Input from "../components/input";
 import { useRouter } from "next/navigation";
-
-function isTokenExpired(token: string) {
-  try {
-    const decoded: any = jwt_decode(token);
-    if (decoded.exp && Date.now() >= decoded.exp * 1000) {
-      return true;
-    }
-    return false;
-  } catch (err) {
-    console.error("Erro ao decodificar o token:", err);
-    return true;
-  }
-}
+import { getCookiServer } from "@/lib/cookieServer";
 
 export default function Category() {
-  const router = useRouter();
+  async function handleRegisterCategory(formData: FormData) {
+    const name = formData.get("name");
 
-  async function handleRegisterCategory(
-    event: React.FormEvent<HTMLFormElement>
-  ) {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const name = formData.get("name")?.toString();
-
-    console.log("Nome obtido do formulário:", name);
-
-    if (!name) {
-      console.error("Nome não preenchido.");
-      return;
-    }
+    if (name === "") console.log(name);
 
     const data = {
       name: name
     };
 
-    const token = getCookie("token");
+    const token = await getCookiServer();
 
-    console.log("Token obtido dos cookies:", token);
-
-    if (!token) {
-      console.error(
-        "Token não encontrado. Certifique-se de que o usuário está autenticado e o token foi salvo corretamente nos cookies."
-      );
-      return;
-    }
-
-    if (isTokenExpired(token)) {
-      console.error("Token expirado. O usuário precisa autenticar novamente.");
-      return;
-    }
-
-    try {
-      console.log("Dados a serem enviados:", data);
-      console.log("Token utilizado na requisição:", `Bearer ${token}`);
-      await api.post("/category", data, {
+    const response = await api
+      .post("/category", data, {
         headers: {
           Authorization: `Bearer ${token}`
         }
+      })
+      .catch((err) => {
+        console.log(err);
       });
-      console.log("Categoria registrada com sucesso");
-      router.push("/dashboard");
-    } catch (err) {
-      console.error("Erro ao tentar registrar categoria:", err);
-    }
+
+    console.log(response);
   }
 
   return (
@@ -77,7 +36,7 @@ export default function Category() {
         <h1 className="text-3xl font-bold text-white mb-6">Nova categoria</h1>
         <form
           className="flex flex-col space-y-4"
-          onSubmit={handleRegisterCategory}
+          action={handleRegisterCategory}
         >
           <Input
             name="name"
